291aa05bf377335f917b8fbd4601003a5898d901
trilliongame <99936172+coffee-logo@users.noreply.github.com>
2024-05-08 01:29:09 +0900

    Update SampleScriptA.cs

--- Assets/SampleScriptA.cs
+++ Assets/SampleScriptA.cs
@@ -7,7 +7,7 @@ public class SampleScriptA : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Adding a sample explanation
     }
 
     // Update is called once per frame
