5fdf6689eea68bf7c20a231601692357cd913a97
trilliongame <99936172+coffee-logo@users.noreply.github.com>
2024-05-09 16:54:14 +0900

    Update Player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,26 @@
 using UnityEngine;
 using UnityEngine.AI;
 
-
+//Player라는 클래스를 새롭게 만든다. 그리고 MonoBehaviour라는 클래스를 상속한다
 public class Player : MonoBehaviour
 {
+    //SpriteRenderer라는 클래스에 해당하는 객체인 spriteRenderer를 새롭게 형성한다
     private SpriteRenderer spriteRenderer;
+    //게임에서 실제로 사용할 sprite 들을 모아 sprites라는 이름의 리스트를 제작한다. public으로 설정함으로써 inspector창에서 수정할 수 있게 하고 다른 곳에서도 불러올 수 있도록 한다
     public Sprite[] sprites;
+    //SpriteIndex라는 새로운 변수를 만든다. 단, 본 변수는 int라는 자료형을 지닌다
     private int SpriteIndex;
+    //direction 이라는 새로운 변수를 만든다. 단, 본 변수는 Vector3 클래스의 인스턴스이다. 
     private Vector3 direction;
+    //gravity라는 새로운 변수를 만든다. 본 변수는 float라는 자료형을 지닌다. 기본값은 -9.8f이다
     public float gravity = -9.8f;
+    //strength라는 새로운 변수를 만든다. 본 변수는 float라는 자료형을 지닌다. 기본값은 5f이다.
     public float strength = 5f;
 
+    //Awake라는 새로운 메소드를 만든다. 플레이어가 처음 활성화됐을 때 가장 먼저 실행되는 메소드이다. 
     private void Awake()
     {
+        //현재 게임 오브젝트에서 SpriteRenderer 컴포넌트를 찾아 그것을 spriteRenderer 변수에 할당
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
